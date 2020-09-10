2285dfb7c86729a56dfb6f401991b68c4f28be6b
Niels Lund Kragbak <niels_kragbak@hotmail.com>
2020-09-10 15:19:15 +0200

    index content

--- byoh/main/static/main/script.js
+++ byoh/main/static/main/script.js
@@ -1,3 +1,10 @@
 // document.querySelector("#header-user")
 
 // alert('yo');
+
+function nav()
+   {
+   var w = document.myform.mylist.selectedIndex;
+   var url_add = document.myform.mylist.options[w].value;
+   window.location.href = url_add;
+   }
